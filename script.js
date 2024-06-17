dc65a31d608b302e3fdb40954c6b794b73103334
DevUnanam <riaahh20@yahoo.com>
2024-06-17 04:36:18 -0700

    First  commit.

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('not-interested').addEventListener('click', function() {
+    if (window.opener) {
+        window.close();
+    } else {
+        alert("Thank you for your response. You can close this tab now.");
+    }
+});
+
+document.getElementById('accept-invitation').addEventListener('click', function() {
+    window.location.href = 'https://chat.whatsapp.com/IPLvbVa47qUIBSmVzH5u9l';
+});
