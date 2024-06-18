d4c1e52275c572aa39f82165eedf2e2b6d8e6a34
DevUnanam <riaahh20@yahoo.com>
2024-06-18 08:32:06 -0700

    More corrections

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// user is prompted to close the tab
 document.getElementById('not-interested').addEventListener('click', function() {
     if (window.opener) {
         window.close();
@@ -6,6 +7,24 @@ document.getElementById('not-interested').addEventListener('click', function() {
     }
 });
 
+// user gets redirected to the WhatsApp group
 document.getElementById('accept-invitation').addEventListener('click', function() {
     window.location.href = 'https://chat.whatsapp.com/I18hDa06Muv9MSKPlehHUx';
+});
+
+// Change button content on click
+document.getElementById('not-interested').addEventListener('mousedown', function() {
+    this.innerHTML = "😢";
+});
+
+document.getElementById('not-interested').addEventListener('mouseup', function() {
+    this.innerHTML = "Tap here if not interested";
+});
+
+document.getElementById('accept-invitation').addEventListener('mousedown', function() {
+    this.innerHTML = "😊";
+});
+
+document.getElementById('accept-invitation').addEventListener('mouseup', function() {
+    this.innerHTML = "Tap here if you accept my invitation";
 });
